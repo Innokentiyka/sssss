50f904aef565f4969eb63499e88e1cadc3bf4b9e
PC <kerik006@mail.ru>
2023-10-07 13:34:51 +0300

    h

---
+++ main.py
@@ -0,0 +1,6 @@
+# Import webbrowser module in the program
+import webbrowser
+# Add a URL of JavaTpoint to open it in a browser
+url= 'https://gist.github.com/oscarbotru/0b968db25008c34c2579100bb3220e11'
+# Open the URL using open() function of module
+webbrowser.open_new_tab(url)
